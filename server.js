8a1b23ea73249adc3718b31bfdafe1d93b4103bf
Shivam Maheshwari <shivammaheshwari503@gmail.com>
2024-02-20 06:14:42 +0530

    frontend url

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ const server = app.listen(PORT, console.log(`Server has started on ${PORT} and $
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chat-frontend-sand.vercel.app",
   },
 });
 
